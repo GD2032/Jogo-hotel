b2c77b089f8a16f164c8247390c76ff835f40ddf
gabriel dias santos <gabrieldsantos@fac.pe.senac.br>
2023-12-04 20:44:10 -0300

    sprites iniciais

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 80.0
 const JUMP_VELOCITY = -400.0
 
+# Get Player Scale on X axis
+var x = scale.x
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
+func _process(_delta):
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.stop()
+	
+	if Input.is_action_just_pressed("right") and scale.x > 0:
+		scale = Vector2(-x, scale.y)
+		print(x)
+	elif Input.is_action_just_pressed("left") and scale.x < 0:
+		scale = Vector2(-x, scale.y) 
+		print(x)
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():

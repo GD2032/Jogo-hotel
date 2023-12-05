5366da7c7ba77424434827d3369143d6d49236f7
gabriel dias santos <gabrieldsantos@fac.pe.senac.br>
2023-12-04 23:54:58 -0300

    area 2d

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,35 +9,38 @@ var x = scale.x
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var _animated_sprite = $AnimatedSprite2D
+var Direction = "Right"
 
+func ChangeDirection():
+	if Direction == "Left":
+		get_node( "AnimatedSprite2D" ).set_flip_h( false )
+		Direction = "Right"
+	elif Direction == "Right":
+		get_node( "AnimatedSprite2D" ).set_flip_h( true )
+		Direction = "Left"
+		
 func _process(_delta):
 	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
 		_animated_sprite.play("walk")
 	else:
 		_animated_sprite.stop()
 	
-	if Input.is_action_just_pressed("right") and scale.x > 0:
-		scale = Vector2(-x, scale.y)
-		print(x)
-	elif Input.is_action_just_pressed("left") and scale.x < 0:
-		scale = Vector2(-x, scale.y) 
-		print(x)
-		
+	if Input.is_action_just_pressed("right") and Direction == "Left":
+		ChangeDirection()
+	elif Input.is_action_just_pressed("left") and Direction == "Right":
+		ChangeDirection()
+	
+	
+	
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
